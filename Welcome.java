5b37f21d5ad7dec0a951769241285dc49f5e461b
Vernong132 <vgage@hawkmail.hccfl.edu>
2024-12-01 23:07:14 -0500

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,8 @@
+public class Welcome {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+
+    }
+}
